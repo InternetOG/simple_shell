1da12dfaf67ef04a1b151d886f092dd788bdab49
InternetOG <omoleyeosadare@gmail.com>
2023-09-21 01:13:42 +0100

    happyOG

---
+++ functions.c
@@ -0,0 +1,119 @@
+#include "shell.h"
+
+void remove_nwl(char *str)
+{
+        int i;
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                if (str[i] == '\n')
+                {
+                        break;
+                }
+        }
+        str[i] = '\0';
+}
+
+void remove_cmt(char *str)
+{
+        int i = 0;
+        if (str[i] == '#')
+        {
+                str[i] = '\0';
+        }
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                if (str[i] == '#' && str[i - 1] == ' ')
+                {
+                        break;
+                }
+        }
+        str[i] = '\0';
+}
+
+char **token(char *str, char *delim)
+{
+        int numDelim = 0;
+        char **argv = NULL;
+        char * tok = NULL;
+        char * sPtr = NULL;
+        tok = strtok_r(str, delim, &sPtr);
+        argv = (char **)(malloc(BUFFER_SIZE * sizeof(char *)));
+        if (argv != NULL)
+        {
+                for (numDelim = 0; tok != NULL; numDelim++)
+                {
+                        argv[numDelim] = (char *)malloc(sizeof(char) * 1024);
+                        if (argv[numDelim] != NULL)
+                        {
+                                argv[numDelim] = tok;
+                                tok = strtok_r(NULL, delim, &sPtr);
+                        }
+                }
+                argv[numDelim] = NULL;
+        }
+		return (argv);
+}
+
+char *get_envron(char *path)
+{
+        char **env;
+        char * pPtr;
+        char * path_copy;
+
+        env = environ;
+        while (*env != NULL)
+        {
+                pPtr = *env;
+                path_copy = path;
+                while (*pPtr == *path_copy)
+                {
+                        if (*pPtr == '=')
+                        {
+                                break;
+                        }
+                        pPtr++;
+                        path_copy++;
+                }
+                if ((*pPtr == '=') && (*path_copy == '\0'))
+                {
+                        return (pPtr + 1);
+                }
+                env++;
+        }
+        return (NULL);
+}
+
+char *path_chk(char *cmd)
+{
+        char **arrPath = NULL;
+        char *path = get_envron("PATH");
+        char *pHolder, *pHolder1, *pathDup;
+        int cnt = 0;
+        if (path == NULL || strlen(path) == 0)
+		{
+			return (NULL);
+		}
+
+        pathDup = (char *)malloc(sizeof(char) * (strlen(path) + 1));
+        strcpy(path, pathDup);
+        arrPath = token(pathDup, ":");
+        while (arrPath[cnt] != NULL)
+        {
+                pHolder1 = strcat(arrPath[cnt], "/");
+                pHolder = strcat(pHolder1, cmd);
+                if (access(pHolder, F_OK) == 0)
+                {
+                        free(pHolder1);
+                        free(arrPath);
+                        free(pathDup);
+						return (pHolder);
+                }
+                free(pHolder);
+                free(pHolder1);
+
+                cnt++;
+        }
+        free(pathDup);
+        free(arrPath);
+        return (NULL);
+}
---
+++ shell.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+char **cmds = NULL;
+char *buff = NULL;
+char *sh_name = NULL;
+int check_status = 0;
+
+
+int main(int argc, char *argv[])
+{
+        char **curr_cmd = NULL;
+        int i, cmd_type = 0;
+        size_t cnt = 0;
+
+        (void) argc;
+
+        signal(SIGINT, ctrl_c);
+        sh_name = argv[0];
+        while (1)
+        {
+                non_interactive();
+                _printf(" ($) ", 1);
+                if (getline(&buff, &cnt, stdin) == -1)
+                {
+                        free(buff);
+                        exit(check_status);
+                }
+                remove_nwl(buff);
+                remove_cmt(buff);
+                cmds = token(buff, ";");
+                i = 0;
+                while (cmds[i] != NULL)
+                {
+                        curr_cmd = token(cmds[i], " ");
+                        if (curr_cmd[0] == NULL)
+                        {
+                                free(curr_cmd);
+                                break;
+                        }
+                        cmd_type = parse_cmd(curr_cmd[0]);
+                        init(curr_cmd, cmd_type);
+                        free(curr_cmd);
+						i++;
+                }
+                free(cmds);
+        }
+        free(buff);
+        return (check_status);
+}
